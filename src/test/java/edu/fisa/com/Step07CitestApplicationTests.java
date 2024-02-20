1a7358667e4933cf686bda1135c5883cc12e1b23
popododo0720 <popododo0720@naver.com>
2024-02-20 15:11:16 +0900

    commit

---
+++ src/test/java/edu/fisa/com/Step07CitestApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fisa.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Step07CitestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
